c752d6d056eccd710363f2b14c6d622b3f6544e9
dojh111 <dojh111@gmail.com>
2020-06-04 17:55:08 +0800

    Icons Added

--- components/screen-components/homescreen-list/FlavorText.js
+++ components/screen-components/homescreen-list/FlavorText.js
@@ -1,5 +1,16 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import {
+  Ionicons,
+  Entypo,
+  FontAwesome,
+  FontAwesome5,
+  MaterialCommunityIcons,
+} from "@expo/vector-icons";
+import { Row } from "native-base";
+
+//Track timing
+let time = <Ionicons name="md-moon" size={29} color="gold" />;
 
 const Greetings = {
   early: "Fancy some late-night supper, ",
@@ -10,16 +21,19 @@ const Greetings = {
 };
 
 const timeGreeting = (name) => {
-  let hours = new Date().getHours();
+  let hours = 12;//new Date().getHours();
   let greeting = Greetings.evening + name + ".";
   if (hours >= 22 || hours <= 4) {
     greeting = Greetings.early + name + "?";
   } else if (hours >= 5 && hours <= 10) {
     greeting = Greetings.morning + name + "!";
+    time = <Feather name="sun" size={29} color="gold" />;
   } else if (hours >= 11 && hours <= 14) {
     greeting = Greetings.lunch + name + ".";
+    time = <MaterialCommunityIcons name="food" size={29} color="crimson" />;
   } else if (hours >= 15 && hours <= 17) {
     greeting = Greetings.tea;
+    time = <MaterialCommunityIcons name="tea" size={29} color="indianred" />;
   }
 
   return greeting;
@@ -28,8 +42,11 @@ const timeGreeting = (name) => {
 const FlavorText = ({ name }) => {
   const greeting = timeGreeting(name);
   return (
-    <View>
-      <Text style={styles.heading}>{greeting}</Text>
+    <View style={styles.container}>
+      <Text style={styles.heading}>{greeting} </Text>
+      <View style={styles.icon}>
+        {time}
+      </View>
     </View>
   );
 };
@@ -42,4 +59,11 @@ const styles = StyleSheet.create({
     fontSize: 29,
     marginTop: 10,
   },
+  container: {
+    flexDirection: "row",
+  },
+  icon: {
+    marginTop: 15,
+    marginLeft: 5,
+  }
 });
