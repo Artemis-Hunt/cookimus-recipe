7c78f2fd57a801711478c1f17d62f8ca62a5bff6
dojh111 <dojh111@gmail.com>
2020-06-04 16:55:14 +0800

    Delete Added

--- data/RecipeList.js
+++ data/RecipeList.js
@@ -7,21 +7,25 @@ const RecipeList = [
           name: "Flour",
           amount: 2,
           unit: "cups",
+          key: "Flour00"
         },
         {
           name: "Yeast",
           amount: 12,
           unit: "grams",
+          key: "Yeast01"
         },
         {
           name: "Eggs",
           amount: 1,
           unit: "",
+          key: "Eggs02"
         },
         {
           name: "Water",
           amount: 150,
           unit: "ml",
+          key: "Water03"
         },
       ],
     },
@@ -32,21 +36,25 @@ const RecipeList = [
           name: "Flour",
           amount: 2,
           unit: "cups",
+          key: "Flour10"
         },
         {
           name: "Yeast",
           amount: 12,
           unit: "grams",
+          key: "Yeast11"
         },
         {
           name: "Eggs",
           amount: 1,
           unit: "",
+          key: "Eggs12"
         },
         {
           name: "Water",
           amount: 150,
           unit: "ml",
+          key: "Water13"
         },
       ],
     },
@@ -57,21 +65,25 @@ const RecipeList = [
           name: "Flour",
           amount: 2,
           unit: "cups",
+          key: "Flour20"
         },
         {
           name: "Yeast",
           amount: 12,
           unit: "grams",
+          key: "Yeast21"
         },
         {
           name: "Eggs",
           amount: 1,
           unit: "",
+          key: "Eggs22"
         },
         {
           name: "Water",
           amount: 150,
           unit: "ml",
+          key: "Water23"
         },
       ],
     },
