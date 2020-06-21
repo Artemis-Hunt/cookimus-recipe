fd1b40914da3ab2d32b1e86d248aac91ab042eef
dojh111 <dojh111@gmail.com>
2020-06-21 11:34:36 +0800

    Added Fraction Conversion

--- components/screen-components/grocery-list/Item.js
+++ components/screen-components/grocery-list/Item.js
@@ -1,16 +1,30 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
-
 import { FontAwesome } from '@expo/vector-icons'; 
+const frac = require('frac');
 
-const Item = ({ title, amounts, units }) => (
+const Item = ({ title, amounts, units }) => {
+  let fracArray = frac(amounts, 9, true);
+  let final = 0;
+  //Conversion to fractions
+  if(fracArray[2] === 1) {
+    final = amounts;
+  } else {
+    if(fracArray[0] === 0) {
+      final = fracArray[1].toString() + '/' + fracArray[2].toString();
+    }
+    else {
+      final = fracArray[0].toString() + ' ' + fracArray[1].toString() + '/' + fracArray[2].toString();
+    }
+  }
+  return(
   <View style={styles.ingredientEntry}>
     <Text style={[styles.ingredientText, styles.text]}><FontAwesome name="circle-thin" size={17} color="coral" />  {title}</Text>
     <Text style={[styles.ingredientValueText, styles.text]}>
-      {amounts} {units}
+      {final} {units}
     </Text>
   </View>
-);
+)};
 
 const styles = StyleSheet.create({
   //For each ingredient entry
