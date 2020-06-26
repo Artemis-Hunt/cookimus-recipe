c0179bca87ba11678e9b38b23b368fc16e76bdfd
dojh111 <dojh111@gmail.com>
2020-06-26 21:25:09 +0800

    Shifted Hash Functions

---
+++ components/screen-components/grocery-list/HashFunctions.js
@@ -0,0 +1,206 @@
+import React, { Component } from "react";
+
+import RecipeList from '../../../data/RecipeList.js'
+import HashTable from '../../../data/HashTable.js'
+import SavedRecipes from "../../../data/SavedRecipes.js"
+
+//Size of hash table
+const ArraySize = 100;
+const Multiplier = 37;
+
+export default class HashFunctions extends Component {
+    constructor(props) {
+        super(props);
+    }
+    //Hashes the passed in string in item and returns the key
+    hashFunction = (item) => {
+        let total = 0;
+
+        for (let i = 0; i < item.length; i++) {
+            total += Multiplier * total + item.charCodeAt(i);
+        }
+        total %= ArraySize;
+        return total;
+    };
+
+    //Function to capitalise first letter of all leading words in string
+    capitaliseString = (str) => {
+        for (let k = 0; k < str.length; k++) {
+            str[k] =
+                str[k][0].toUpperCase() + str[k].substr(1); //Appends everything else from index 1 onwards
+        }
+        //Combine back
+        return str.join(" ");
+    };
+
+    //Pass in single recipes at a time in recipe list format, add into hash table
+    //Outer to loop through all the individual Recipes - Currently loops from start of list
+    combineFunction = (index) => {
+        for (let i = 0; i < index; i++) {
+            //Loop through all the individual ingredients in each recipe
+            for (let j = 0; j < RecipeList[i].data.length; j++) {
+                //Split ingredient into different parts if more than 1 word and capitalise all starting
+                let newItem = RecipeList[i].data[j].name;
+                let splitName = newItem.split(" ");
+                let newName = this.capitaliseString(splitName);
+
+                //Hash combinedItem
+                let key = this.hashFunction(newName);
+
+                //Add item into hash table
+                this.insertIntoHash(i, j, newName, key);
+            }
+        }
+        //Move all items from hash table into the combined list
+        this.props.rebuildList();
+    };
+
+    //This function will add to combined list single items - No need to loop entire list
+    handleSingleItem = (newSingleItem, itemIndex) => {
+        let splitName = newSingleItem.split(" ");
+        let newName = this.capitaliseString(splitName);
+        let key = this.hashFunction(newName);
+        this.insertIntoHash(RecipeList.length - 1, itemIndex, newName, key);
+        this.props.rebuildList();
+    };
+
+    //This function will handle the item added to the combined list
+    insertIntoHash = (i, j, itemName, key) => {
+        //Handling item slotting/collisions
+        let collision = 0;
+        let hashIndex = (key + collision) % ArraySize;
+        while (collision !== ArraySize) {
+            if (
+                (HashTable[hashIndex].name === null ||
+                    HashTable[hashIndex].deleted === 1) &&
+                HashTable[hashIndex].name !== itemName
+            ) {
+                //Space in hashtable is empty, set as new object in hashTable - Currently dosent attend to different units
+                HashTable[hashIndex].name = itemName;
+                HashTable[hashIndex].amount = Number(RecipeList[i].data[j].amount);
+                HashTable[hashIndex].unit = RecipeList[i].data[j].unit;
+                HashTable[hashIndex].deleted = 0;
+                return;
+            } else if (HashTable[hashIndex].name === itemName) {
+                //Same Item, add amounts
+                HashTable[hashIndex].amount += Number(RecipeList[i].data[j].amount);
+                return;
+            }
+            collision++;
+            hashIndex = (key + collision) % ArraySize;
+        }
+        if (collision === ArraySize) alert("Error, array full");
+    };
+
+    //Delte function for list
+    deleteItem = (id) => {
+        //Split id into name / recipe index / ingredient index
+        let [name, recipeIndex, ingrIndex] = id.split(".");
+        this.hashDelete(recipeIndex, ingrIndex, true);
+        RecipeList[recipeIndex].data[ingrIndex].mark = false;
+        //Remove ingredient from RecipeList. Update keys for ingredients after deleted ingredient
+        RecipeList[recipeIndex].data.splice(ingrIndex, 1);
+        for (let j = ingrIndex; j < RecipeList[recipeIndex].data.length; j++) {
+            RecipeList[recipeIndex].data[j].key = this.generateKey(recipeIndex, j);
+        }
+        //Remove Title from RecipeList
+        if (
+            RecipeList[recipeIndex].data.length === 0 &&
+            RecipeList[recipeIndex].title !== "Added to list"
+        ) {
+            //Clear Savedrecipe
+            for (let i = 0; i < SavedRecipes.length; i++) {
+                if (RecipeList[recipeIndex].title === SavedRecipes[i].title) {
+                    SavedRecipes.splice(i, 1);
+                    break;
+                }
+            }
+            RecipeList.splice(recipeIndex, 1);
+            this.oldLength = RecipeList.length;
+            this.bulkGenerateKey(recipeIndex);
+        }
+    };
+
+    //Delete entire recipe at once
+    deleteSection = (title) => {
+        let index = 0;
+        //Finding index of the recipe in recipelist
+        for (let i of RecipeList) {
+            if (i.title === title) { break; }
+            index++;
+        }
+        //Bulk delete of all ingredients found in recipe
+        for (let j = 0; j < RecipeList[index].data.length; j++) {
+            RecipeList[index].data[j].mark = false;
+            this.hashDelete(index, j, false);
+        }
+        //Delete entire entry
+        if (RecipeList[index].title !== "Added to list") {
+            for (let i = 0; i < SavedRecipes.length; i++) {
+                if (RecipeList[index].title === SavedRecipes[i].title) {
+                    SavedRecipes.splice(i, 1);
+                    break;
+                }
+            }
+            RecipeList.splice(index, 1);
+            this.oldLength = RecipeList.length;
+            this.bulkGenerateKey(index);
+        } else {
+            //Delete all except title for added to list
+            RecipeList[index].data.splice(0, RecipeList[index].data.length);
+        }
+        this.props.rebuildList();
+    }
+    //Deletes items from the hashtable to update combined list
+    hashDelete = (recipeIndex, ingrIndex, rebuildFlag) => {
+        //Reference the item to be deleted
+        let toDelete = RecipeList[recipeIndex].data[ingrIndex];
+        let splitName = toDelete.name.split(" ");
+        let newName = this.capitaliseString(splitName);
+
+        //Update hash table. Delete entry if required
+        let hashKey = this.hashFunction(newName);
+        let collision = 0;
+        let hashIndex = (hashKey + collision) % ArraySize;
+        while (collision !== ArraySize) {
+            if (HashTable[hashIndex].name === newName) {
+                HashTable[hashIndex].amount -= toDelete.amount;
+                if (HashTable[hashIndex].amount <= 0) {
+                    HashTable[hashIndex].name = null;
+                    HashTable[hashIndex].amount = "";
+                    HashTable[hashIndex].unit = "";
+                    HashTable[hashIndex].deleted = 1;
+                    HashTable[hashIndex].mark = false;
+                }
+                break;
+            }
+            collision++;
+            hashIndex = (hashKey + collision) % ArraySize;
+        }
+        if (rebuildFlag === true) {
+            this.props.rebuildList();
+        } else {
+            return;
+        }
+    }
+
+    //Generate keys in bulk
+    bulkGenerateKey(startIndex) {
+        for (let i = startIndex; i < RecipeList.length; i++) {
+            for (let j = 0; j < RecipeList[i].data.length; j++) {
+                //Generate keys for each item
+                RecipeList[i].data[j].key = this.generateKey(i, j);
+            }
+        }
+    }
+
+    generateKey = (i, j) => {
+        let key = `${RecipeList[i].data[j].name}.${i}.${j}`;
+        return key;
+    };
+
+    render() {
+        return (null);
+    }
+
+}
--- components/screens/GroceryList.js
+++ components/screens/GroceryList.js
@@ -17,16 +17,16 @@ import PortionModal from "../screen-components/grocery-list/PortionModal.js";
 import RecipeList from "../../data/RecipeList";
 import CombinedList from "../../data/CombinedList.js";
 import HashTable from "../../data/HashTable.js";
-import SavedRecipes from "../../data/SavedRecipes.js"
 import scrapedList from "../../data/allRecipesScraped.json"
 import Animated, { diff } from "react-native-reanimated";
 import { MaterialCommunityIcons, Entypo } from '@expo/vector-icons';
 
 import { SwipeListView } from 'react-native-swipe-list-view';
 
+import HashFunctions from "../screen-components/grocery-list/HashFunctions.js";
+
 //Size of hash table
 const ArraySize = 100;
-const Multiplier = 37;
 
 const ItemSeparator = () => {
   return <View style={styles.separator} />;
@@ -35,7 +35,6 @@ const ItemSeparator = () => {
 export default class GroceryList extends Component {
   constructor(props) {
     super(props);
-    this.key;
     this.oldLength = RecipeList.length;
     this.state = {
       showComponent: false,
@@ -56,12 +55,18 @@ export default class GroceryList extends Component {
     this.sendPortion = this.sendPortion.bind(this);
     this.forceUpdate = this.forceUpdate.bind(this);
     this.rebuildCombinedList = this.rebuildCombinedList.bind(this);
+    this.callCombineFunction = this.callCombineFunction.bind(this);
+    this.callBulkGenerate = this.callBulkGenerate.bind(this);
+    this.callgenerateKey = this.callgenerateKey.bind(this);
+    this.callhandleSingleItem = this.callhandleSingleItem.bind(this);
+    this.callDeleteItem = this.callDeleteItem.bind(this);
+    this.callDeleteSection = this.callDeleteSection.bind(this);
   }
 
   //Run combine list on startup
   componentDidMount() {
-    this.combineFunction(RecipeList.length);
-    this.bulkGenerateKey(0);
+    this.callCombineFunction(RecipeList.length);
+    this.callBulkGenerate(0);
     //Trigger a re-render whenever the grocery list tab is pressed
     this.unsubscribe = this.props.navigation.addListener("tabPress", (e) => {
       this.forceUpdate();
@@ -74,8 +79,8 @@ export default class GroceryList extends Component {
       //Update new table, add to combined list
       let index = newLength - this.oldLength;
       this.oldLength = newLength;
-      this.combineFunction(index);
-      this.bulkGenerateKey(0);
+      this.callCombineFunction(index);
+      this.callBulkGenerate(0);
       this.forceUpdate();
     }
   }
@@ -83,16 +88,6 @@ export default class GroceryList extends Component {
   componentWillUnmount() {
     //Unsubscribe event handler
     //this.unsubscribe();
-  }
-
-  //Generate keys in bulk
-  bulkGenerateKey(startIndex) {
-    for (let i = startIndex; i < RecipeList.length; i++) {
-      for (let j = 0; j < RecipeList[i].data.length; j++) {
-        //Generate keys for each item
-        RecipeList[i].data[j].key = this.generateKey(i, j);
-      }
-    }
   }
 
   showAddItem() {
@@ -129,11 +124,6 @@ export default class GroceryList extends Component {
     this.setState({ units: item });
   };
 
-  generateKey = (i, j) => {
-    let key = `${RecipeList[i].data[j].name}.${i}.${j}`;
-    return key;
-  };
-
   //Functions for modal
   showModal = () => {
     this.refs.portionModal.renderModal();
@@ -142,97 +132,24 @@ export default class GroceryList extends Component {
     this.refs.portionModal.receivePortion(portion, title);
   }
 
-  //Deletes items from the hashtable to update combined list
-  hashDelete = (recipeIndex, ingrIndex, rebuildFlag) => {
-    //Reference the item to be deleted
-    let toDelete = RecipeList[recipeIndex].data[ingrIndex];
-    let splitName = toDelete.name.split(" ");
-    let newName = this.capitaliseString(splitName);
-
-    //Update hash table. Delete entry if required
-    let hashKey = this.hashFunction(newName);
-    let collision = 0;
-    let hashIndex = (hashKey + collision) % ArraySize;
-    while (collision !== ArraySize) {
-      if (HashTable[hashIndex].name === newName) {
-        HashTable[hashIndex].amount -= toDelete.amount;
-        if (HashTable[hashIndex].amount <= 0) {
-          HashTable[hashIndex].name = null;
-          HashTable[hashIndex].amount = "";
-          HashTable[hashIndex].unit = "";
-          HashTable[hashIndex].deleted = 1;
-          HashTable[hashIndex].mark = false;
-        }
-        break;
-      }
-      collision++;
-      hashIndex = (hashKey + collision) % ArraySize;
-    }
-    if (rebuildFlag === true) {
-      this.rebuildCombinedList();
-    } else {
-      return;
-    }
+  //Functions to call hashFunctions
+  callCombineFunction = (index) => {
+    this.refs.hashFunctions.combineFunction(index);
   }
-
-  //Delte function for list
-  deleteItem = (id) => {
-    //Split id into name / recipe index / ingredient index
-    let [name, recipeIndex, ingrIndex] = id.split(".");
-    this.hashDelete(recipeIndex, ingrIndex, true);
-    RecipeList[recipeIndex].data[ingrIndex].mark = false;
-    //Remove ingredient from RecipeList. Update keys for ingredients after deleted ingredient
-    RecipeList[recipeIndex].data.splice(ingrIndex, 1);
-    for (let j = ingrIndex; j < RecipeList[recipeIndex].data.length; j++) {
-      RecipeList[recipeIndex].data[j].key = this.generateKey(recipeIndex, j);
-    }
-    //Remove Title from RecipeList
-    if (
-      RecipeList[recipeIndex].data.length === 0 &&
-      RecipeList[recipeIndex].title !== "Added to list"
-    ) {
-      //Clear Savedrecipe
-      for (let i = 0; i < SavedRecipes.length; i++) {
-        if (RecipeList[recipeIndex].title === SavedRecipes[i].title) {
-          SavedRecipes.splice(i, 1);
-          break;
-        }
-      }
-      RecipeList.splice(recipeIndex, 1);
-      this.oldLength = RecipeList.length;
-      this.bulkGenerateKey(recipeIndex);
-    }
-  };
-
-  //Delete entire recipe at once
-  deleteSection = (title) => {
-    let index = 0;
-    //Finding index of the recipe in recipelist
-    for (let i of RecipeList) {
-      if (i.title === title) { break; }
-      index++;
-    }
-    //Bulk delete of all ingredients found in recipe
-    for (let j = 0; j < RecipeList[index].data.length; j++) {
-      RecipeList[index].data[j].mark = false;
-      this.hashDelete(index, j, false);
-    }
-    //Delete entire entry
-    if (RecipeList[index].title !== "Added to list") {
-      for (let i = 0; i < SavedRecipes.length; i++) {
-        if (RecipeList[index].title === SavedRecipes[i].title) {
-          SavedRecipes.splice(i, 1);
-          break;
-        }
-      }
-      RecipeList.splice(index, 1);
-      this.oldLength = RecipeList.length;
-      this.bulkGenerateKey(index);
-    } else {
-      //Delete all except title for added to list
-      RecipeList[index].data.splice(0, RecipeList[index].data.length);
-    }
-    this.rebuildCombinedList();
+  callBulkGenerate = (startIndex) => {
+    this.refs.hashFunctions.bulkGenerateKey(startIndex);
+  }
+  callgenerateKey = (recipeIndex, itemIndex) => {
+    this.refs.generateKey(recipeIndex, itemIndex);
+  }
+  callhandleSingleItem = (name, index) => {
+    this.refs.hashFunctions.handleSingleItem(name, index);
+  }
+  callDeleteItem = (key) => {
+    this.refs.hashFunctions.deleteItem(key);
+  }
+  callDeleteSection = (title) => {
+    this.refs.hashFunctions.deleteSection(title);
   }
 
   //Check if entered is valid
@@ -250,55 +167,16 @@ export default class GroceryList extends Component {
       let itemIndex = RecipeList[RecipeIndex].data.length;
 
       RecipeList[RecipeIndex].data.push(newObject);
-      RecipeList[RecipeIndex].data[itemIndex].key = this.generateKey(
+      RecipeList[RecipeIndex].data[itemIndex].key = this.callgenerateKey(
         RecipeIndex,
         itemIndex
       );
 
-      this.handleSingleItem(newObject.name, itemIndex);
+      this.callhandleSingleItem(newObject.name, itemIndex);
 
       //Clear fields
       this.setState({ name: "", quantity: "", units: "", incompleteField: "" });
     } else this.setState({ incompleteField: "Please fill in all fields" });
-  };
-
-  //Hashes the passed in string in item and returns the key
-  hashFunction = (item) => {
-    let total = 0;
-
-    for (let i = 0; i < item.length; i++) {
-      total += Multiplier * total + item.charCodeAt(i);
-    }
-    total %= ArraySize;
-    return total;
-  };
-
-  //This function will handle the item added to the combined list
-  insertIntoHash = (i, j, itemName) => {
-    //Handling item slotting/collisions
-    let collision = 0;
-    let hashIndex = (this.key + collision) % ArraySize;
-    while (collision !== ArraySize) {
-      if (
-        (HashTable[hashIndex].name === null ||
-          HashTable[hashIndex].deleted === 1) &&
-        HashTable[hashIndex].name !== itemName
-      ) {
-        //Space in hashtable is empty, set as new object in hashTable - Currently dosent attend to different units
-        HashTable[hashIndex].name = itemName;
-        HashTable[hashIndex].amount = Number(RecipeList[i].data[j].amount);
-        HashTable[hashIndex].unit = RecipeList[i].data[j].unit;
-        HashTable[hashIndex].deleted = 0;
-        return;
-      } else if (HashTable[hashIndex].name === itemName) {
-        //Same Item, add amounts
-        HashTable[hashIndex].amount += Number(RecipeList[i].data[j].amount);
-        return;
-      }
-      collision++;
-      hashIndex = (this.key + collision) % ArraySize;
-    }
-    if (collision === ArraySize) alert("Error, array full");
   };
 
   //This function adds all the items in the hashtable into the combinedlist
@@ -313,47 +191,6 @@ export default class GroceryList extends Component {
     }
   };
 
-  //This function will add to combined list single items - No need to loop entire list
-  handleSingleItem = (newSingleItem, itemIndex) => {
-    let splitName = newSingleItem.split(" ");
-    let newName = this.capitaliseString(splitName);
-    this.key = this.hashFunction(newName);
-    this.insertIntoHash(RecipeList.length - 1, itemIndex, newName);
-    this.rebuildCombinedList();
-  };
-
-  //Function to capitalise first letter of all leading words in string
-  capitaliseString = (str) => {
-    for (let k = 0; k < str.length; k++) {
-      str[k] =
-        str[k][0].toUpperCase() + str[k].substr(1); //Appends everything else from index 1 onwards
-    }
-    //Combine back
-    return str.join(" ");
-  };
-
-  //Pass in single recipes at a time in recipe list format, add into hash table
-  //Outer to loop through all the individual Recipes - Currently loops from start of list
-  combineFunction = (index) => {
-    for (let i = 0; i < index; i++) {
-      //Loop through all the individual ingredients in each recipe
-      for (let j = 0; j < RecipeList[i].data.length; j++) {
-        //Split ingredient into different parts if more than 1 word and capitalise all starting
-        let newItem = RecipeList[i].data[j].name;
-        let splitName = newItem.split(" ");
-        let newName = this.capitaliseString(splitName);
-
-        //Hash combinedItem
-        this.key = this.hashFunction(newName);
-
-        //Add item into hash table
-        this.insertIntoHash(i, j, newName);
-      }
-    }
-    //Move all items from hash table into the combined list
-    this.rebuildCombinedList();
-  };
-
   closeRow = (rowMap, rowKey) => {
     if (rowMap[rowKey]) {
       rowMap[rowKey].closeRow();
@@ -365,7 +202,7 @@ export default class GroceryList extends Component {
       <View style={styles.hiddenItem}>
         <TouchableOpacity
           onPress={() => {
-            this.deleteItem(data.item.key);
+            this.callDeleteItem(data.item.key);
             this.closeRow(rowMap, data.item.key);
             this.forceUpdate();
           }}
@@ -456,7 +293,7 @@ export default class GroceryList extends Component {
                 <View style={styles.titleCard}>
                   <TouchableOpacity
                     onPress={() => {
-                      this.deleteSection(title);
+                      this.callDeleteSection(title);
                       this.forceUpdate();
                     }}
                   >
@@ -488,6 +325,10 @@ export default class GroceryList extends Component {
         <PortionModal
           ref={'portionModal'}
           MainRefresh={this.forceUpdate}
+          rebuildList={this.rebuildCombinedList}
+        />
+        <HashFunctions
+          ref={'hashFunctions'}
           rebuildList={this.rebuildCombinedList}
         />
       </View >
