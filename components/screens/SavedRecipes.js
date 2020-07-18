be8ed37c8a619af214048c93482c6486cd618c24
Artemis-Hunt <stevetheguy@naver.com>
2020-07-18 14:32:02 +0800

    Initial saved recipes tab

---
+++ components/screens/SavedRecipes.js
@@ -0,0 +1,17 @@
+import React from "react"
+import {View, Text, StyleSheet} from "react-native"
+
+const SavedRecipes = () => {
+    return(
+        <View>
+            <Text>Test</Text>
+        </View>
+    )
+}
+
+const styles = StlyeSheet.create({
+    container: {
+      flex: 1,  
+    },
+})
+export default SavedRecipes;
